e0ca4750854a0b3e6f714613cfdd49219d147ee2
ASWANTH P P <ppaswanth3@gmail.com>
2018-04-06 16:09:05 +0530

    directory structure modified

---
+++ src/POX-Detections/detectionUsingPCA.py
@@ -0,0 +1,87 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import statsmodels.formula.api as smf
+import math
+from numpy import linalg as LA
+
+class PCA(object):
+      def covariance(x, y, dataX, dataY):
+	  sum = 0
+	  for i in range(0, len(dataX)):
+	      sum = sum + (x - dataX[i]) * (y - dataY[i])
+	  sum = sum / len(dataX)
+	  return sum
+	
+      sum = 0
+      dataX = []
+      dataY = []
+      covmat = []
+
+      data = pd.read_csv('test.csv', index_col = 0)
+
+      for i in range(1, len(data.x) + 1):
+	  dataX.append(data.x[i])
+	  dataY.append(data.y[i])
+
+      lm = smf.ols(formula = 'y ~ x', data = data).fit()
+      xmid = (data.x.min() + data.x.max()) / 2
+      X_new = pd.DataFrame({'x': [xmid]})
+      ymid = lm.predict(X_new)[0]
+
+      graph = pd.DataFrame({'x': [data.x.min(), data.x.max()]})
+      preds = lm.predict(graph)
+      data.plot(kind = 'scatter', x = 'x', y = 'y')
+
+      # plot principal component axis
+      plt.plot(graph, preds, c = 'red', linewidth = 2)
+      array = []
+
+      for i in range(1, len(data.x) + 1):
+	  temp = math.sqrt(pow(data.x[i] - xmid, 2) + pow(data.y[i] - ymid, 2))
+	  array.append(temp)
+
+      covmat.append(covariance(xmid, xmid, dataX, dataX))
+      covmat.append(covariance(xmid, ymid, dataX, dataY))
+      covmat.append(covariance(ymid, xmid, dataY, dataX))
+      covmat.append(covariance(ymid, ymid, dataY, dataY))
+
+      print "\n\n\tCovariance matrix:"
+      print "\t-------------------------\n"
+      print "\t",covmat[0],"   \t",covmat[1]
+      print "\t",covmat[2],"   \t",covmat[3]
+
+      a1 = covmat[0]
+      a2 = covmat[1]
+      a3 = covmat[2]
+      a4 = covmat[3]
+
+      eigen1 = (a1 + a4 + math.sqrt(pow((a1 + a4), 2) - 4 * (a1 * a4 - a2 * a3))) / 2
+      eigen2 = (a1 + a4 - math.sqrt(pow((a1 + a4), 2) - 4 * (a1 * a4 - a2 * a3))) / 2
+
+      print "\n\n\tEigenvalues:"
+      print "\t--------------\n"
+      print "\t",eigen1
+      print "\t",eigen2
+      
+      #v = LA.eig(np.array(a1,a2,a3,a4))
+      
+      print "Eigen vector"
+      print v
+      
+      '''
+      b1=a1-eigen1
+      b2=a2
+      b3=a3
+      b4=a4-eigen1
+      
+      # need to calculate first eigenvector
+      
+      c1=a1-eigen2
+      c2=a2
+      c3=a3
+      c4=a4-eigen2
+      
+      # need to calculate second eigenvector
+      '''
+      plt.title('Principal Component Axis')
+      plt.show()
---
+++ src/POX-Detections/l3_detection_pca.py
@@ -0,0 +1,324 @@
+import os
+import datetime
+from pox.core import core
+import pox
+
+from pox.lib.packet.ethernet import ethernet, ETHER_BROADCAST
+from pox.lib.packet.ipv4 import ipv4
+from pox.lib.packet.arp import arp
+from pox.lib.addresses import IPAddr, EthAddr
+from pox.lib.util import str_to_bool, dpid_to_str
+from pox.lib.recoco import Timer
+
+import pox.openflow.libopenflow_01 as of
+
+from pox.lib.revent import *
+import itertools 
+import time
+
+from .detection import PCA
+diction = {}
+ent_obj = PCA()   
+set_Timer = False     
+defendDDOS=False      
+
+log = core.getLogger() 
+FLOW_IDLE_TIMEOUT = 10   
+ARP_TIMEOUT = 60 * 2    
+MAX_BUFFERED_PER_IP = 5      
+MAX_BUFFER_TIME = 5
+
+class Entry (object):
+  def __init__ (self, port, mac):
+    self.timeout = time.time() + ARP_TIMEOUT
+    self.port = port
+    self.mac = mac
+
+  def __eq__ (self, other):
+    if type(other) == tuple:
+      return (self.port,self.mac)==other
+    else:
+      return (self.port,self.mac)==(other.port,other.mac)
+  def __ne__ (self, other):
+    return not self.__eq__(other)
+
+  def isExpired (self):
+    if self.port == of.OFPP_NONE: return False
+    return time.time() > self.timeout
+
+def dpid_to_mac (dpid):
+  return EthAddr("%012x" % (dpid & 0xffFFffFFffFF,))
+
+class l3_switch (EventMixin):
+  def __init__ (self, fakeways = [], arp_for_unknowns = False, wide = False):
+    self.fakeways = set(fakeways)
+
+    self.wide = wide
+
+    self.arp_for_unknowns = arp_for_unknowns
+
+    self.outstanding_arps = {}
+
+    self.lost_buffers = {}
+
+    self.arpTable = {}
+
+    self._expire_timer = Timer(5, self._handle_expiration, recurring=True)
+
+    core.listen_to_dependencies(self)
+
+  def _handle_expiration (self):
+    empty = []
+    for k,v in self.lost_buffers.iteritems():
+      dpid,ip = k
+
+      for item in list(v):
+        expires_at,buffer_id,in_port = item
+        if expires_at < time.time():
+          v.remove(item)
+          po = of.ofp_packet_out(buffer_id = buffer_id, in_port = in_port)
+          core.openflow.sendToDPID(dpid, po)
+      if len(v) == 0: empty.append(k)
+  
+    for k in empty:
+      del self.lost_buffers[k]
+
+  def _send_lost_buffers (self, dpid, ipaddr, macaddr, port):
+    if (dpid,ipaddr) in self.lost_buffers:
+      bucket = self.lost_buffers[(dpid,ipaddr)]
+      del self.lost_buffers[(dpid,ipaddr)]
+      log.debug("Sending %i buffered packets to %s from %s"
+                % (len(bucket),ipaddr,dpid_to_str(dpid)))
+      for _,buffer_id,in_port in bucket:
+        po = of.ofp_packet_out(buffer_id=buffer_id,in_port=in_port)
+        po.actions.append(of.ofp_action_dl_addr.set_dst(macaddr))
+        po.actions.append(of.ofp_action_output(port = port))
+        core.openflow.sendToDPID(dpid, po)
+
+  def _handle_openflow_PacketIn (self, event):
+    dpid = event.connection.dpid
+    inport = event.port
+    packet = event.parsed
+    global set_Timer
+    global defendDDOS
+    global blockPort
+    timerSet =False
+    global diction
+    def preventing():
+      global diction
+      global set_Timer
+      if not set_Timer:
+        set_Timer =True
+
+      print"\n\n*********new packetIN************"
+      if len(diction) == 0:
+        print("Empty diction ",str(event.connection.dpid), str(event.port))
+        diction[event.connection.dpid] = {}
+        diction[event.connection.dpid][event.port] = 1
+      elif event.connection.dpid not in diction:
+        diction[event.connection.dpid] = {}
+        diction[event.connection.dpid][event.port] = 1
+      else:
+        if event.connection.dpid in diction:
+          if event.port in diction[event.connection.dpid]:
+            temp_count=0
+            temp_count =diction[event.connection.dpid][event.port]
+            temp_count = temp_count+1
+            diction[event.connection.dpid][event.port]=temp_count
+            print "*****************************************************************************************************************************************************************************"
+            print "printing dpid port number and its packet count: ",  str(event.connection.dpid), str(diction[event.connection.dpid]), str(diction[event.connection.dpid][event.port])
+            print "*****************************************************************************************************************************************************************************"
+          else:
+            diction[event.connection.dpid][event.port] = 1
+    
+    def _timer_func ():
+      global diction
+      global set_Timer
+      
+      if set_Timer==True:
+        for k,v in diction.iteritems():
+          for i,j in v.iteritems():
+            if j >=1:
+              print "_____________________________________________________________________________________________"
+              print "\n",datetime.datetime.now(),"*******    DDOS DETECTED   ********"
+              print "\n",str(diction)
+              print "\n",datetime.datetime.now(),": BLOCKED PORT NUMBER  : ", str(i), " OF SWITCH ID: ", str(k)
+              print "\n____________________________________________________________________________________________"
+              os._exit(0)
+              dpid = k
+              msg = of.ofp_packet_out(in_port=i)
+              core.openflow.sendToDPID(dpid,msg)          
+      diction={}
+    
+    if not packet.parsed:
+      log.warning("%i %i ignoring unparsed packet", dpid, inport)
+      return
+
+    if dpid not in self.arpTable:
+      self.arpTable[dpid] = {}
+      for fake in self.fakeways:
+        self.arpTable[dpid][IPAddr(fake)] = Entry(of.OFPP_NONE,
+         dpid_to_mac(dpid))
+
+    if packet.type == ethernet.LLDP_TYPE:
+      return
+
+    if isinstance(packet.next, ipv4):
+      log.debug("%i %i IP %s => %s", dpid,inport, packet.next.srcip,packet.next.dstip)
+      ent_obj.statcolect(event.parsed.next.dstip)
+      if ent_obj.value <1.0:
+        preventing()
+        if timerSet is not True:
+         Timer(1, _timer_func, recurring=True)
+         timerSet=False
+      else:
+        timerSet=False
+
+      self._send_lost_buffers(dpid, packet.next.srcip, packet.src, inport)
+
+      if packet.next.srcip in self.arpTable[dpid]:
+        if self.arpTable[dpid][packet.next.srcip] != (inport, packet.src):
+          log.info("%i %i RE-learned %s", dpid,inport,packet.next.srcip)
+          if self.wide:
+            msg = of.ofp_flow_mod(command=of.OFPFC_DELETE)
+            msg.match.nw_dst = packet.next.srcip
+            msg.match.dl_type = ethernet.IP_TYPE
+            event.connection.send(msg)
+      else:
+        log.debug("%i %i learned %s", dpid,inport,packet.next.srcip)
+      self.arpTable[dpid][packet.next.srcip] = Entry(inport, packet.src)
+
+      dstaddr = packet.next.dstip
+      if dstaddr in self.arpTable[dpid]:
+
+        prt = self.arpTable[dpid][dstaddr].port
+        mac = self.arpTable[dpid][dstaddr].mac
+        if prt == inport:
+          log.warning("%i %i not sending packet for %s back out of the "
+                      "input port" % (dpid, inport, dstaddr))
+        else:
+          log.debug("%i %i installing flow for %s => %s out port %i"
+                    % (dpid, inport, packet.next.srcip, dstaddr, prt))
+
+          actions = []
+          actions.append(of.ofp_action_dl_addr.set_dst(mac))
+          actions.append(of.ofp_action_output(port = prt))
+          if self.wide:
+            match = of.ofp_match(dl_type = packet.type, nw_dst = dstaddr)
+          else:
+            match = of.ofp_match.from_packet(packet, inport)
+
+          msg = of.ofp_flow_mod(command=of.OFPFC_ADD,
+                                idle_timeout=FLOW_IDLE_TIMEOUT,
+                                hard_timeout=of.OFP_FLOW_PERMANENT,
+                                buffer_id=event.ofp.buffer_id,
+                                actions=actions,
+                                match=match)
+          event.connection.send(msg.pack())
+      elif self.arp_for_unknowns:
+        if (dpid,dstaddr) not in self.lost_buffers:
+          self.lost_buffers[(dpid,dstaddr)] = []
+        bucket = self.lost_buffers[(dpid,dstaddr)]
+        entry = (time.time() + MAX_BUFFER_TIME,event.ofp.buffer_id,inport)
+        bucket.append(entry)
+        while len(bucket) > MAX_BUFFERED_PER_IP: del bucket[0]
+
+        self.outstanding_arps = {k:v for k,v in
+         self.outstanding_arps.iteritems() if v > time.time()}
+
+        if (dpid,dstaddr) in self.outstanding_arps:
+          return
+
+        self.outstanding_arps[(dpid,dstaddr)] = time.time() + 4
+
+        r = arp()
+        r.hwtype = r.HW_TYPE_ETHERNET
+        r.prototype = r.PROTO_TYPE_IP
+        r.hwlen = 6
+        r.protolen = r.protolen
+        r.opcode = r.REQUEST
+        r.hwdst = ETHER_BROADCAST
+        r.protodst = dstaddr
+        r.hwsrc = packet.src
+        r.protosrc = packet.next.srcip
+        e = ethernet(type=ethernet.ARP_TYPE, src=packet.src,
+                     dst=ETHER_BROADCAST)
+        e.set_payload(r)
+        log.debug("%i %i ARPing for %s on behalf of %s" % (dpid, inport,
+         r.protodst, r.protosrc))
+        msg = of.ofp_packet_out()
+        msg.data = e.pack()
+        msg.actions.append(of.ofp_action_output(port = of.OFPP_FLOOD))
+        msg.in_port = inport
+        event.connection.send(msg)
+
+    elif isinstance(packet.next, arp):
+      a = packet.next
+      log.debug("%i %i ARP %s %s => %s", dpid, inport,
+       {arp.REQUEST:"request",arp.REPLY:"reply"}.get(a.opcode,
+       'op:%i' % (a.opcode,)), a.protosrc, a.protodst)
+
+      if a.prototype == arp.PROTO_TYPE_IP:
+        if a.hwtype == arp.HW_TYPE_ETHERNET:
+          if a.protosrc != 0:
+
+            if a.protosrc in self.arpTable[dpid]:
+              if self.arpTable[dpid][a.protosrc] != (inport, packet.src):
+                log.info("%i %i RE-learned %s", dpid,inport,a.protosrc)
+                if self.wide:
+                  msg = of.ofp_flow_mod(command=of.OFPFC_DELETE)
+                  msg.match.dl_type = ethernet.IP_TYPE
+                  msg.match.nw_dst = a.protosrc
+                  event.connection.send(msg)
+            else:
+              log.debug("%i %i learned %s", dpid,inport,a.protosrc)
+            self.arpTable[dpid][a.protosrc] = Entry(inport, packet.src)
+
+            self._send_lost_buffers(dpid, a.protosrc, packet.src, inport)
+
+            if a.opcode == arp.REQUEST:
+
+              if a.protodst in self.arpTable[dpid]:
+
+                if not self.arpTable[dpid][a.protodst].isExpired():
+
+                  r = arp()
+                  r.hwtype = a.hwtype
+                  r.prototype = a.prototype
+                  r.hwlen = a.hwlen
+                  r.protolen = a.protolen
+                  r.opcode = arp.REPLY
+                  r.hwdst = a.hwsrc
+                  r.protodst = a.protosrc
+                  r.protosrc = a.protodst
+                  r.hwsrc = self.arpTable[dpid][a.protodst].mac
+                  e = ethernet(type=packet.type, src=dpid_to_mac(dpid),
+                               dst=a.hwsrc)
+                  e.set_payload(r)
+                  log.debug("%i %i answering ARP for %s" % (dpid, inport,
+                   r.protosrc))
+                  msg = of.ofp_packet_out()
+                  msg.data = e.pack()
+                  msg.actions.append(of.ofp_action_output(port =
+                                                          of.OFPP_IN_PORT))
+                  msg.in_port = inport
+                  event.connection.send(msg)
+                  return
+
+      log.debug("%i %i flooding ARP %s %s => %s" % (dpid, inport,
+       {arp.REQUEST:"request",arp.REPLY:"reply"}.get(a.opcode,
+       'op:%i' % (a.opcode,)), a.protosrc, a.protodst))
+
+      msg = of.ofp_packet_out(in_port = inport, data = event.ofp,
+          action = of.ofp_action_output(port = of.OFPP_FLOOD))
+      event.connection.send(msg)
+
+def launch (fakeways="", arp_for_unknowns=None, wide=False):
+  fakeways = fakeways.replace(","," ").split()
+  fakeways = [IPAddr(x) for x in fakeways]
+  if arp_for_unknowns is None:
+    arp_for_unknowns = len(fakeways) > 0
+  else:
+    arp_for_unknowns = str_to_bool(arp_for_unknowns)
+  core.registerNew(l3_switch, fakeways, arp_for_unknowns, wide)
+  
