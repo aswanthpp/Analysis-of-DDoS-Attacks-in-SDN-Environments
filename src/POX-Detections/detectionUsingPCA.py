b6f4db869e72ed42e076fec5f81832c947c5aadd
ASWANTH P P <ppaswanth3@gmail.com>
2018-04-13 23:39:30 +0530

    ip to integer calculation added

--- src/POX-Detections/detectionUsingPCA.py
+++ src/POX-Detections/detectionUsingPCA.py
@@ -23,24 +23,32 @@ class PCA(object):
 	      sum = sum + (x - dataX[i]) * (y - dataY[i])
 	  sum = sum / len(dataX)
 	  return sum
-      def updateMean(s,d)
-      		if(count!=0):
-      			meanSrc=(meanSrc*(count-1)+s)/count
-      			meanDst=(meanDst*(count-1)+s)/count
+	  
+      def ipToNum(ip):
+      	  
+      	  ipOctet=ip.split(".")
+      	  ipNum=(int(ipOctet[0])*pow(256,3))+ (int(ipOctet[1])*pow(256,2)) + (int(ipOctet[2])*pow(256,1)) + (int(ipOctet[3])*pow(256,0))
+          return ipNum
+          
+      def updateMean(s,d):
+      	  if(count!=0):
+      	     meanSrc=(meanSrc*(count-1)+s)/count
+      	     meanDst=(meanDst*(count-1)+s)/count
 
-      def statcolect(self, srcIp, dstIp):
-        self.count +=1
-        tempSrc=srcIp.split(".")
-        tempDst=dstIp.split(".")
+      def collectStats(self, srcIp, dstIp):
+          self.count +=1
+          srcIpNum=ipToNum(srcIp)
+          dstIpNum=ipToNum(dstIp)
         
-	self.srcIpList.append(int(tempSrc[-1]))
-	self.dstIpList.append(int(tempDst[-1])))
-	updateMean(srcIp,dstIp)
-	a1=covariance(meanSrc, meanSrc, srcIpList, srcIpList)
-        a2=covariance(meanSrc, meanDst, srcIpList, dstIpList)
-        a3=covariance(meanDst, meanSrc, dstIpList, srcIpList)
-        a4=covariance(meanDst, meanDst, dstIpList, dstIpList)
-        eigenValues, eigenVector = LA.eig(np.array([[a1, a2], [a3, a4]]))
+	  self.srcIpList.append(srcIpNum)
+	  self.dstIpList.append(dstIpNum)
+	  updateMean(srcIp,dstIp)
+	  a1=covariance(meanSrc, meanSrc, srcIpList, srcIpList)
+          a2=covariance(meanSrc, meanDst, srcIpList, dstIpList)
+          a3=covariance(meanDst, meanSrc, dstIpList, srcIpList)
+          a4=covariance(meanDst, meanDst, dstIpList, dstIpList)
+          eigenValues, eigenVector = LA.eig(np.array([[a1, a2], [a3, a4]]))
+          print "Eigen vector ",eigenVector
 	
 
       
