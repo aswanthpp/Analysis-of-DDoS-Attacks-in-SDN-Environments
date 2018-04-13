a3bb68c9c5edb443419e31d94181748433cd458e
ASWANTH P P <ppaswanth3@gmail.com>
2018-04-14 00:54:27 +0530

    object error resolved

--- src/POX-Detections/detectionUsingPCA.py
+++ src/POX-Detections/detectionUsingPCA.py
@@ -1,9 +1,27 @@
+import numpy as np
 from numpy import linalg as LA
 
 import math
 from pox.core import core
 
 log = core.getLogger()
+
+def ipToNum(ip):      	  
+  ipNum = 0
+  j=0
+  ipOctet=str(ip).split(".")
+  for i in ipOctet :
+     ipNum += int(i)*pow(256,3-j)
+     j=j+1
+  return ipNum
+
+def covariance(x, y, dataX, dataY):
+	  sum = 0
+	  for i in range(0, len(dataX)):
+	      sum = sum + (x - dataX[i]) * (y - dataY[i])
+	  sum = sum / len(dataX)
+	  return sum
+
 
 class PCA(object):
 
@@ -16,44 +34,29 @@ class PCA(object):
       meanDst = 0
       srcDict = {}
       dstDict = {}
-
-      def covariance(x, y, dataX, dataY):
-	  sum = 0
-	  for i in range(0, len(dataX)):
-	      sum = sum + (x - dataX[i]) * (y - dataY[i])
-	  sum = sum / len(dataX)
-	  return sum
-	  
-      def ipToNum(ip):
-      	  ipNum = 0
-      	  j=0
-      	  ipOctet=ip.split(".")
-      	  for i in ipOctet :
-      	   ipNum += int(i)*pow(256,3-j)
-      	   j=j+1
-          return ipNum
-          
-      def updateMean(s,d):
-      	  if(count!=0):
-      	     meanSrc=(meanSrc*(count-1)+s)/count
-      	     meanDst=(meanDst*(count-1)+s)/count
-
+     
+      def updateMean(self,s,d):
+      	  if(self.count!=0):
+      	     self.meanSrc=(self.meanSrc*(self.count-1)+s)/self.count
+      	     self.meanDst=(self.meanDst*(self.count-1)+s)/self.count    
+      
       def collectStats(self, srcIp, dstIp):
           self.count +=1
-          print "-------------",srcIp,"---d--",dstIp
           srcIpNum=ipToNum(srcIp)
           dstIpNum=ipToNum(dstIp)
         
 	  self.srcIpList.append(srcIpNum)
 	  self.dstIpList.append(dstIpNum)
-	  updateMean(srcIp,dstIp)
-	  a1=covariance(meanSrc, meanSrc, srcIpList, srcIpList)
-          a2=covariance(meanSrc, meanDst, srcIpList, dstIpList)
-          a3=covariance(meanDst, meanSrc, dstIpList, srcIpList)
-          a4=covariance(meanDst, meanDst, dstIpList, dstIpList)
+	  self.updateMean(srcIpNum,dstIpNum)
+	  a1=covariance(self.meanSrc, self.meanSrc, self.srcIpList, self.srcIpList)
+          a2=covariance(self.meanSrc, self.meanDst, self.srcIpList, self.dstIpList)
+          a3=covariance(self.meanDst, self.meanSrc, self.dstIpList, self.srcIpList)
+          a4=covariance(self.meanDst, self.meanDst, self.dstIpList, self.dstIpList)
+          
           eigenValues, eigenVector = LA.eig(np.array([[a1, a2], [a3, a4]]))
           print "Eigen vector ",eigenVector
-	
+      def __init__(self):
+	  pass
 
       
 	
